bbe0f2eff7a168e3373f2364690ef10a4d80e0af
MustaphaKareem@KomaConnect <mustaphakareem16@gmail.com>
2022-06-22 14:10:22 +0100

    add the sepearte .js file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Mustapha, 5.8ft, Nigeria");
+let name = "Kareem Mustapha"
+let height = "5.8 ft"
+let country = "Nigeria"
+
+console.log(name);
+console.log(height);
+console.log(country);
